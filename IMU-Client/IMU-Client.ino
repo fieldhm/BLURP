5f91520d879a1ec88e1a5f02da25f609de2e5aae
fieldhm <hfield@mit.edu>
2017-01-23 19:08:06 -0500

    Add files via upload

---
+++ IMU-Client/IMU-Client.ino
@@ -0,0 +1,171 @@
+#include <Wifi_S08_v2.h>
+
+
+
+#define SSID "BLURP"
+#define PASSWD "MOMENTUM"
+
+#define POLLPERIOD 0
+#define BLOCKCHECK 100
+#define WAIT 100
+
+ESP8266 wifi = ESP8266(0,false);
+
+String MAC;
+unsigned long lastRequest = 0;
+
+int led = 13;
+
+int digLeft = 14;
+int analogLeft = 21;
+int digRight = 15;
+int analogRight = 20;
+
+
+
+
+void setup() {
+  pinMode(digLeft, OUTPUT);
+  pinMode(digRight, OUTPUT);
+  
+  Serial.begin(115200);
+  wifi.begin();
+  MAC = wifi.getMAC();
+  wifi.connectWifi(SSID, PASSWD);
+  while (!wifi.isConnected()); //wait for connection
+  Serial.println("wifi connection succesfull");
+
+  //check blocking
+  pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH);
+}
+
+int count = 0;
+
+int SPEED = 128;
+
+int beginInd, endInd;
+
+bool forward, backward, counterclockwise, clockwise;
+
+void loop() {
+
+  getValues();
+  
+    if(forward) // going forward
+    {
+      while(!backward){
+       //Serial.println("f");
+        setMotors(1,1);
+        getValues();
+        }
+      stopAndWait();
+    }
+
+    else if(clockwise) // turn clockwise
+    {
+      while(!counterclockwise){
+     //   Serial.println("r");
+        setMotors(1,0);
+              getValues();
+        }
+        stopAndWait();
+    }
+
+    else if(backward) // going backward
+    {
+      while(!forward){
+      //  Serial.println("b");
+        setMotors(0,0);
+        getValues();
+        }
+        stopAndWait();
+    }
+
+    else if(counterclockwise){ //turn counterclockwise
+      while(!clockwise){
+     //   Serial.println("l");
+        setMotors(0,1);
+        getValues();
+        }
+        stopAndWait();
+      } 
+
+    else {
+      digitalWrite(digLeft, LOW); //irrelevant
+      digitalWrite(digRight, LOW); //irrelevant
+      analogWrite(analogLeft, 0);
+      analogWrite(analogRight, 0);
+      }
+      
+    
+ }
+
+
+
+/*********************** helper functions ********************/
+
+char values[500];
+void getValues(){ 
+  
+    String response;
+    
+    forward = 0;
+    backward = 0;
+    clockwise = 0;
+    counterclockwise = 0;
+    
+  if(wifi.hasResponse()){
+      response = wifi.getResponse();
+      
+      beginInd = response.indexOf("~");
+      endInd =response.lastIndexOf("~");
+      response = response.substring(beginInd+1, endInd);
+      forward = response.charAt(0) == '1';
+      backward = response.charAt(1) == '1';
+      clockwise = response.charAt(2) == '1';
+      counterclockwise = response.charAt(3) == '1';      
+      Serial.println(response);
+  }
+    
+    if (!wifi.isBusy() && millis()-lastRequest > POLLPERIOD) {
+      String domain = "192.168.4.1";
+      String path = "/"; 
+  
+      sprintf(values, "BoolValuesSentFromClient:f=%d,b=%d,cw=%d,ccw=%d", forward, backward, clockwise, counterclockwise);
+      Serial.println(values);
+      wifi.sendRequest(GET, domain, 80, path, values);
+      lastRequest = millis();
+    }      
+
+}
+
+void setMotors(int left, int right){
+      digitalWrite(digLeft, left); //irrelevant
+      digitalWrite(digRight, right); //irrelevant
+  
+      analogWrite(analogLeft, SPEED);
+      analogWrite(analogRight, SPEED);
+      }
+
+void stopAndWait(){
+  Serial.println("stop and wait");
+      digitalWrite(digLeft, LOW); //irrelevant
+      digitalWrite(digRight, LOW); //irrelevant
+      analogWrite(analogLeft, 0);
+      analogWrite(analogRight, 0);
+      delay(WAIT);
+      if(wifi.hasResponse()){
+        wifi.getResponse();
+      }
+      Serial.println("wait over, returning to loop");
+      digitalWrite(led, LOW);
+      delay(500);
+      digitalWrite(led, HIGH);
+      delay(500);
+      digitalWrite(led, LOW);
+      delay(500);
+      digitalWrite(led, HIGH);
+      
+}
+
---
+++ IMU-Server/IMU-Server.ino
@@ -0,0 +1,107 @@
+/* Server side computation of direcitons */
+
+
+#include <Wifi_S08_v2.h>
+#include "MPU9250.h"
+MPU9250 imu;
+
+#define SSID "BLURP"
+#define PASSWD "MOMENTUM"
+#define UPDATEPERIOD 10
+
+
+
+//variables needed for getting and computing direction
+int UDval;
+int LRval;
+float SCALE = 500;
+int HIT_VAL = 40;
+int UD_LOW = -1*HIT_VAL;
+int UD_HIGH = HIT_VAL;
+int LR_LOW = -1*HIT_VAL;
+int LR_HIGH = HIT_VAL;
+
+bool forward, backward, counterclockwise, clockwise;
+//************************
+
+ESP8266 wifi = ESP8266(1,false);
+String html = "<html>\n<title>It works!</title>\n<body>\n<h1>Congrats</h1>\n<p>You have successfully interneted.</p>\n</body>\n</html>";
+int val = 0;
+
+int light = 0; 
+int led = 13;
+
+int lastupdate = millis();
+int blinktime = millis();
+
+char html2[500];
+
+void setup() {
+  Serial.begin(115200);
+  
+/****** Wifi Setup*****************/
+  wifi.begin();
+  Serial.println("startserver");
+  wifi.startserver(SSID, PASSWD);
+  Serial.println("setup complete");
+  wifi.setPage("/",html);
+/********************************/
+
+  /**************** IMU Setup***********************/
+  Wire.begin();
+  byte c = imu.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
+  Serial.print("MPU9250 "); Serial.print("I AM "); Serial.print(c, HEX);
+  Serial.print(" I should be "); Serial.println(0x73, HEX);
+  if (c == 0x71) // WHO_AM_I   //IF YOU HAVE A BLUE IMU CHANGE TO 0x73
+  {
+    Serial.println("MPU9250 is online...");
+       Serial.println("MPU9250 is online...");
+    // Calibrate gyro and accelerometers, load biases in bias registers
+    imu.calibrateMPU9250(imu.gyroBias, imu.accelBias);
+    } // if (c == 0x73)
+  else
+  {
+    Serial.print("Could not connect to MPU9250: 0x");
+    Serial.println(c, HEX);
+    while(1) ; // Loop forever if communication doesn't happen
+  }
+  /*****************************************/
+}
+
+char getDirectionValue[500];
+
+void loop() {
+
+  int m = millis() - lastupdate;
+  if(m > UPDATEPERIOD){
+    val++;
+
+    strcpy(getDirectionValue,getDirection());
+    sprintf(html2,"<html>\n<body>\n<p>%s</p>\n</body>\n</html>", getDirectionValue);
+    wifi.setPage("/",html2);
+    Serial.println(getDirectionValue);
+    lastupdate = millis();
+  //Serial.println(wifi.getData());
+  }
+}
+
+char* getDirection(){
+
+     char boolValues[500]; 
+     
+     imu.readGyroData(imu.gyroCount);
+     UDval = imu.gyroCount[0];
+     UDval = (-1)*(int)UDval/SCALE;
+     LRval = imu.gyroCount[2];
+     LRval = (-1)*(int)LRval/SCALE;
+
+     forward = (UDval > UD_HIGH) &&(LRval<LR_HIGH) && (LRval > LR_LOW) ;
+     clockwise = (LRval > LR_HIGH) && (UDval<UD_HIGH) && (UDval > UD_LOW);
+     backward = (UDval < UD_LOW ) && (LRval<LR_HIGH) && (LRval > LR_LOW);
+     counterclockwise = (LRval < LR_LOW) && (UDval<UD_HIGH) && (UDval > UD_LOW);
+
+    // sprintf(boolValues, "BoolValuesf=%d,b=%d,cw=%d,ccw=%d", forward, backward, clockwise, counterclockwise);
+     sprintf(boolValues, "~%d%d%d%d~", forward, backward, clockwise, counterclockwise);
+     return boolValues;
+}
+
