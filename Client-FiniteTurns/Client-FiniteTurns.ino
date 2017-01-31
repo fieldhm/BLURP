6262146cff59053e2ca3f298b9f367e235025496
fieldhm <hfield@mit.edu>
2017-01-31 17:03:07 -0500

    Add files via upload

---
+++ Client-FiniteTurns/Client-FiniteTurns.ino
@@ -0,0 +1,203 @@
+#include <Wifi_S08_v2.h>
+
+#define SSID "BLURP"
+#define PASSWD "MOMENTUM"
+
+#define POLLPERIOD 0
+#define BLOCKCHECK 100
+#define WAIT 100
+
+ESP8266 wifi = ESP8266(0,false);
+String domain = "192.168.4.1";
+String path = "/"; 
+String MAC;
+unsigned long lastRequest = 0;
+
+/***** pin setup ******/
+#define led 13
+#define digLeft 14
+#define analogLeft 21
+#define digRight 15
+#define analogRight 20
+#define encoder 2 
+/********************/
+
+/*********** Other constants used later on*********/
+#define SPEED 128
+#define NINETY 123
+
+/***** state setup ******/
+typedef enum { STOP, FW, BW, CW, CCW } states;
+//initialize stationary state
+states state = STOP;
+
+/********************/
+
+
+void setup() {
+  pinMode(digLeft, OUTPUT);
+  pinMode(digRight, OUTPUT);
+  
+  Serial.begin(115200);
+  wifi.begin();
+  MAC = wifi.getMAC();
+  wifi.connectWifi(SSID, PASSWD);
+  while (!wifi.isConnected()); //wait for connection
+ // Serial.println("wifi connection succesfull");
+
+  pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH);
+
+  pinMode (encoder,INPUT);
+}
+
+states oldState;
+
+void loop(){
+   if (!wifi.isBusy() && millis()-lastRequest > POLLPERIOD) {
+      wifi.sendRequest(GET, domain, 80, path, "");
+      lastRequest = millis();
+    }
+    
+   while(!wifi.hasResponse());
+    
+    oldState = state;
+    state =  getState(wifi.getResponse());
+    if(oldState != state){
+      Serial.println("New state is: ");
+      Serial.println(state);
+      setMotors();
+      }
+        
+
+}
+
+
+/**** HELPER ****/
+states getState(String response){
+      int beginInd = response.indexOf("~");
+      int endInd =response.lastIndexOf("~");
+      response = response.substring(beginInd+1, endInd);
+      if(response == "FW") return(FW);
+      if(response == "BW") return(BW);
+      if(response == "CW")return(CW);
+      if(response == "CCW")return(CCW);
+      if(response == "STOP")return(STOP);
+      
+  }
+
+void blink(){
+       if(wifi.hasResponse()){
+        wifi.getResponse(); // DO WE NEED THIS????
+      }
+      digitalWrite(led, LOW);
+      delay(500);
+      digitalWrite(led, HIGH);
+      delay(500);
+      digitalWrite(led, LOW);
+      delay(500);
+      digitalWrite(led, HIGH);
+}
+/****************/
+
+
+
+void setMotors(){
+  switch(state){
+    case FW:
+      forward();
+      break;
+    case BW:
+      backward();
+      break;
+    case CW:
+      clockwise();
+      break;
+    case CCW:
+      counterclockwise();
+      break;
+    case STOP:
+      stopmotors();
+      break;
+  }
+}
+
+/***** STATE FUNCTIONS *******/
+void forward(){
+      digitalWrite(digLeft, HIGH); //irrelevant
+      digitalWrite(digRight, HIGH); //irrelevant
+  
+      analogWrite(analogLeft, SPEED);
+      analogWrite(analogRight, SPEED);
+}
+
+void backward(){
+      digitalWrite(digLeft, LOW); //irrelevant
+      digitalWrite(digRight, LOW); //irrelevant
+  
+      analogWrite(analogLeft, SPEED);
+      analogWrite(analogRight, SPEED);
+}
+
+void clockwise(){ //finite turn
+      int encoderLast = LOW;
+      int n = LOW;
+      int encoderPos = 0;
+      
+      digitalWrite(digLeft, HIGH); 
+      digitalWrite(digRight, LOW);
+      analogWrite(analogLeft, SPEED);
+      analogWrite(analogRight, SPEED);
+
+      while(encoderPos < NINETY){
+        n = digitalRead(encoder);
+        if ((encoderLast == LOW) && (n == HIGH)) {
+          encoderPos++;
+        } 
+        encoderLast = n;
+      }
+
+      stopmotors();
+}
+
+  
+
+void counterclockwise(){ //finite turn
+      int encoderLast = LOW;
+      int n = LOW;
+      int encoderPos = 0;
+      
+      digitalWrite(digLeft, LOW); //irrelevant
+      digitalWrite(digRight, HIGH); //irrelevant
+  
+      analogWrite(analogLeft, SPEED);
+      analogWrite(analogRight, SPEED);
+
+      while(encoderPos < NINETY){
+        n = digitalRead(encoder);
+        if ((encoderLast == LOW) && (n == HIGH)) {
+          encoderPos++;
+        } 
+        encoderLast = n;
+      }
+
+      stopmotors();
+}
+
+void stopmotors(){
+      digitalWrite(digLeft, HIGH); //irrelevant
+      digitalWrite(digRight, HIGH); //irrelevant
+  
+      analogWrite(analogLeft, 0);
+      analogWrite(analogRight, 0);
+}
+/*********************************************/
+
+
+
+
+
+
+
+
+
---
+++ Server-FiniteTurns/Server-FiniteTurns.ino
@@ -0,0 +1,203 @@
+/* Server side computation of direcitons */
+
+
+#include <Wifi_S08_v2.h>
+#include "MPU9250.h"
+MPU9250 imu;
+
+#define SSID "BLURP"
+#define PASSWD "MOMENTUM"
+#define UPDATEPERIOD 10
+#define STATEPERIOD 2000 //how long to wait after state change before getting new measurment
+
+
+
+//variables needed for getting and computing direction
+int UDval;
+int LRval;
+float SCALE = 500;
+int HIT_VAL = 40;
+int UD_LOW = -1*HIT_VAL;
+int UD_HIGH = HIT_VAL;
+int LR_LOW = -1*HIT_VAL;
+int LR_HIGH = HIT_VAL;
+
+//************************
+
+
+/***** enum setup ******/
+typedef enum { STOP, FW, BW, CW, CCW } states;
+//initialize state variable
+states state = STOP;
+
+char stateChar[10] = "STOP";
+
+typedef enum { none, fw, bw, cw, ccw } instructs;
+
+instructs instruct = none;
+/********************/
+
+
+ESP8266 wifi = ESP8266(1,false);
+String html = "<html>\n<title>It works!</title>\n<body>\n<h1>Congrats</h1>\n<p>You have successfully interneted.</p>\n</body>\n</html>";
+int val = 0;
+
+int light = 0; 
+int led = 13;
+
+int lastupdate = millis();
+int laststateupdate = millis();
+int blinktime = millis();
+
+char html2[500];
+
+void setup() {
+  Serial.begin(115200);
+  
+/****** Wifi Setup*****************/
+  wifi.begin();
+  Serial.println("startserver");
+  wifi.startserver(SSID, PASSWD);
+  Serial.println("setup complete");
+  wifi.setPage("/",html);
+/********************************/
+
+  /**************** IMU Setup***********************/
+  Wire.begin();
+  byte c = imu.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
+  Serial.print("MPU9250 "); Serial.print("I AM "); Serial.print(c, HEX);
+  Serial.print(" I should be "); Serial.println(0x73, HEX);
+  if (c == 0x71) // WHO_AM_I   //IF YOU HAVE A BLUE IMU CHANGE TO 0x73
+  {
+    Serial.println("MPU9250 is online...");
+       Serial.println("MPU9250 is online...");
+    // Calibrate gyro and accelerometers, load biases in bias registers
+    imu.calibrateMPU9250(imu.gyroBias, imu.accelBias);
+    } // if (c == 0x73)
+  else
+  {
+    Serial.print("Could not connect to MPU9250: 0x");
+    Serial.println(c, HEX);
+    while(1) ; // Loop forever if communication doesn't happen
+  }
+  /*****************************************/
+}
+
+states oldState = STOP;
+
+void loop() {
+
+  int m = millis() - lastupdate;
+  int n = millis()- laststateupdate;
+  
+  if(m > UPDATEPERIOD && n > STATEPERIOD ) //update period counts last time since state changed
+  {
+    val++;
+    updateState();
+    lastupdate = millis();
+    if (oldState != state)// state changed so should update
+    {
+      laststateupdate = millis();
+      sprintf(html2,"<html>\n<title>Current State</title>\n<body>\n<p>~%s~</p>\n</body>\n</html>", stateChar);
+      wifi.setPage("/",html2);
+      oldState = state;
+      Serial.print("Current state: ");
+      Serial.println(stateChar);
+    }
+   wifi.setPage("/",html2); //why does this have to be here????
+  //Serial.println(wifi.getData());
+  }
+}
+
+void getInstruction(){
+       
+     imu.readGyroData(imu.gyroCount);
+     UDval = imu.gyroCount[0];
+     UDval = (-1)*(int)UDval/SCALE;
+     LRval = imu.gyroCount[2];
+     LRval = (-1)*(int)LRval/SCALE;
+
+     if ( (UDval > UD_HIGH) &&(LRval<LR_HIGH) && (LRval > LR_LOW) ) instruct = fw;
+     else if ( (LRval > LR_HIGH) && (UDval<UD_HIGH) && (UDval > UD_LOW)) instruct = cw;
+     else if  ( (UDval < UD_LOW ) && (LRval<LR_HIGH) && (LRval > LR_LOW)) instruct = bw;
+     else if ((LRval < LR_LOW) && (UDval<UD_HIGH) && (UDval > UD_LOW)) instruct = ccw;
+     else instruct = none;
+}
+
+states updateState(){
+  
+  getInstruction();
+  
+  switch (state) {
+    case STOP:
+      switch (instruct) {
+        case fw:
+          state = FW;
+          break;
+        case bw:
+          state = BW;
+          break;
+        case cw:
+          state = CW;
+          break;
+        case ccw:
+          state = CCW;
+          break;
+        default:
+          break;
+      }
+      break;
+    case FW:
+      switch (instruct) {
+        case bw:
+          state = STOP;
+          break;
+        default:
+          break;
+      }
+      break;
+    case BW:
+      switch (instruct) {
+        case fw:
+          state = STOP;
+          break;
+        default:
+          break;
+      }
+      break;
+    case CW:
+      state = STOP;
+      break;
+    case CCW:
+      state = STOP;
+      break;
+    default:
+      break;
+  }   
+  stateToChar();  
+}
+
+void stateToChar(){
+  switch(state){
+    case FW:
+      strcpy(stateChar,"FW");
+      break;
+    case BW:
+      strcpy(stateChar,"BW");
+      break;
+    case CW:
+      strcpy(stateChar,"CW");
+      break;
+    case CCW:
+      strcpy(stateChar,"CCW");
+      break;
+    case STOP:
+      strcpy(stateChar,"STOP");
+      break;
+  }
+
+
+
+  
+}
+
